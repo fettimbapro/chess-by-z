7958da0e440a33415765987c5f397cb853471803
fettimbapro <56511567+fettimbapro@users.noreply.github.com>
2025-08-17 18:48:44 +0200

    feat: autoplay first move for CSV puzzles

--- tests/puzzleOrientation.test.js
+++ tests/puzzleOrientation.test.js
@@ -1,26 +1,12 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 import { PuzzleUI } from "../chess-website-uml/public/src/puzzles/PuzzleUI.js";
+import { Game } from "../chess-website-uml/public/src/core/Game.js";
 
 // Ensure puzzle loading orients board for side to move
 
 test("loadConvertedPuzzle flips orientation", async () => {
-  const game = {
-    fenStr: "",
-    load(f) {
-      this.fenStr = f;
-    },
-    fen() {
-      return this.fenStr;
-    },
-    turn() {
-      return this.fenStr.split(" ")[1];
-    },
-    moveSan() {
-      return {};
-    },
-    undo() {},
-  };
+  const game = new Game();
   let oriented = null;
   const app = {
     sideSel: { value: "white" },
@@ -53,8 +39,9 @@ test("loadConvertedPuzzle flips orientation", async () => {
 
   await puzzles.loadConvertedPuzzle({
     puzzle: { id: "p1", fen: "8/8/8/8/8/8/8/k6K b - - 0 1", moves: "a1b1" },
+    autoplayFirst: true,
   });
-  assert.equal(app.sideSel.value, "black");
-  assert.equal(oriented, "black");
+  assert.equal(app.sideSel.value, "white");
+  assert.equal(oriented, "white");
   assert.equal(app.gameOver, false);
 });
