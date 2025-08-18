81a600da6bacab143cb0ea18079f720afdbe7251
Sam Zlotnik <samzlotnik@MacBookPro.lan>
2025-08-18 02:36:55 +0200

    rm dupes

--- chess-website-uml/public/src/engine/Engine.js
+++
@@ -1,7 +0,0 @@
-// Abstract interface for chess engines
-export class Engine {
-  async analyze(_fen, _opts){ throw new Error('Not implemented'); }
-  async play(_fen, _opts){ throw new Error('Not implemented'); }
-  stop(){ /* optional */ }
-}
-
