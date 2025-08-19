c5685ae82491badeb3c13100a72cdfc7b4bc198a
fettimbapro <56511567+fettimbapro@users.noreply.github.com>
2025-08-19 12:04:38 +0200

    Replace colon events for book moves

--- src/engine/boot.js
+++ src/engine/boot.js
@@ -2,39 +2,43 @@
 // Late-bound bootstrap so we don't have to change your existing App.js.
 // Wires EngineTuner + OpeningBook to the current DOM and engine.
 
-import { EngineAdapter } from './Adapter.js';
-import { EngineTuner } from './EngineTuner.js';
-import { getBookMove } from './OpeningBook.js';
+import { EngineAdapter } from "./Adapter.js";
+import { EngineTuner } from "./EngineTuner.js";
+import { getBookMove } from "./OpeningBook.js";
 
-const $ = (id)=>document.getElementById(id);
+const $ = (id) => document.getElementById(id);
 
-function collectDom(){
+function collectDom() {
   return {
     // controls
-    mode: $('mode'),
-    auto: $('autoTune'),
-    elo: $('elo'), eloVal: $('eloVal'),
-    depth: $('depth'), depthVal: $('depthVal'),
-    multipv: $('multipv'), multipvVal: $('multipvVal'),
+    mode: $("mode"),
+    auto: $("autoTune"),
+    elo: $("elo"),
+    eloVal: $("eloVal"),
+    depth: $("depth"),
+    depthVal: $("depthVal"),
+    multipv: $("multipv"),
+    multipvVal: $("multipvVal"),
     // badges
-    threadsBadge: $('threadsBadge'),
-    hashBadge: $('hashBadge'),
+    threadsBadge: $("threadsBadge"),
+    hashBadge: $("hashBadge"),
     // misc
-    useBook: $('useBook')
+    useBook: $("useBook"),
   };
 }
 
-function initBook(dom){
+function initBook(dom) {
   // Listen to book requests coming from your App (if it emits them)
-  window.addEventListener('book:request', (ev)=>{
-    const { sanHistory="", ply=0, mode='play' } = ev.detail || {};
+  window.addEventListener("book-request", (ev) => {
+    const { sanHistory = "", ply = 0, mode = "play" } = ev.detail || {};
     const enabled = !!dom.useBook?.checked;
     const san = getBookMove({ sanHistory, ply, mode, enabled });
-    if (san) window.dispatchEvent(new CustomEvent('book:move', { detail:{ san } }));
+    if (san)
+      window.dispatchEvent(new CustomEvent("book-move", { detail: { san } }));
   });
 }
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   // Find an engine instance if available
   const adapter = new EngineAdapter(window.app?.engine || window.engine);
   const dom = collectDom();
